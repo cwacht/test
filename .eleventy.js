ff1623cbaa79dd9d0e744d5728f0874acf98814f
Chris Wachtman <cwachtman@gmail.com>
2023-07-01 13:54:49 -0400

    pathPrefix: "/test/"

--- .eleventy.js
+++ .eleventy.js
@@ -67,6 +67,6 @@ module.exports = function (eleventyConfig) {
     templateFormats: ["njk", "md"],
     htmlTemplateEngine: "njk",
     markdownTemplateEngine: "njk",
-    pathPrefix: "/eleventy-base-blog/"
+    pathPrefix: "/test/"
   };
 };
